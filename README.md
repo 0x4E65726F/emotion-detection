e2c26f9439d6b2a1f76aa32f47140464a8a5bd7f
Nero <1109680304lym@gmail.com>
2023-12-07 00:15:20 +0000

    Working on 6 trial

--- README.md
+++ README.md
@@ -1,13 +1,15 @@
 # Install Open CV and PyTorch
-**Install OpenCV: **Install OpenCV using the following command:
+Install OpenCV: Install OpenCV using the following command:
 ```
 conda install -c conda-forge opencv
 ```
-**Install PyTorch: **Install PyTorch run on cpu using the following command:
+Install PyTorch: Install PyTorch run on cpu using the following command:
 ```
 conda install pytorch torchvision torchaudio cpuonly -c pytorch
 ```
 
 
-# How to use Net() function in my module
-For this Net() function, we support
+# Config command for RunPod
+```
+pip install opencv-python matplotlib pandas scikit-learn
+```
--- my_module.py
+++ my_module.py
@@ -40,6 +40,42 @@ class Net(nn.Module):
             cur_channel = fc_channel
         self.fc.append(nn.Linear(cur_channel, img_label))
 
+    def forward(self,x):
+        for conv in self.conv:
+            x = self.pool(F.relu(conv(x)))
+        
+        x = torch.flatten(x, 1) # flatten all dimensions except batch
+
+        for i in range(len(self.fc) - 1):
+            x = F.relu(self.fc[i](x))
+            
+        x = F.softmax(self.fc[len(self.fc) - 1](x), dim=1)
+        return x
+
+class Net2(nn.Module):
+    def __init__(self, img_channel, img_width, img_height, img_label, conv_channels, fc_channels, kernel_size = 5, stride = 1):
+        super().__init__()
+        
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv = nn.ModuleList()
+        self.fc = nn.ModuleList()
+        
+        cur_channel = img_channel
+        cur_width = img_width
+        cur_height = img_height
+        for conv_channel in conv_channels:
+            padding = (conv_channel - 1) // 2
+            cur_width = ((cur_width - kernel_size + 2 * padding) // stride + 1) // 2
+            cur_height = ((cur_height - kernel_size + 2 * padding) // stride + 1) // 2
+            self.conv.append(nn.Conv2d(cur_channel, conv_channel, kernel_size, stride = stride, padding = padding))
+            cur_channel = conv_channel
+            
+        cur_channel = cur_channel * cur_width * cur_height
+        for fc_channel in fc_channels:
+            self.fc.append(nn.Linear(cur_channel, fc_channel))
+            cur_channel = fc_channel
+        self.fc.append(nn.Linear(cur_channel, img_label))
+
         self.dropout = nn.Dropout(0.25)
 
     def forward(self,x):
@@ -54,7 +90,7 @@ class Net(nn.Module):
         x = F.softmax(self.fc[len(self.fc) - 1](x), dim=1)
         return x
 
-def read_pic(data, categories):
+def read_pic(data, categories, limit = -1):
     folder_path = 'images/'
     image_paths = data.iloc[:, 1]
     labels = data.iloc[:, 2].str.lower()
@@ -62,7 +98,11 @@ def read_pic(data, categories):
     label_to_int = {category: i for i, category in enumerate(categories)}
     integer_labels = labels.map(label_to_int)
     y = torch.LongTensor(integer_labels.to_numpy())
+    count = 0
     for filename in image_paths:
+        if count == limit:
+            break
+        count += 1
         if filename.endswith(('.png', '.jpg', '.jpeg')):  # Check for common image file extensions
             img_path = os.path.join(folder_path, filename)
             img = cv2.imread(img_path)
@@ -71,6 +111,8 @@ def read_pic(data, categories):
                 img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
                 X.append(torch.Tensor(img))
     X = torch.stack(X).unsqueeze(1) # convert to pytorch tensor (batch, channels, w, h)
+    if limit > 1:
+        y = y[:limit]
     return X, y
 
 def optimizer_to(optim, device):
@@ -87,17 +129,17 @@ def optimizer_to(optim, device):
                     if subparam._grad is not None:
                         subparam._grad.data = subparam._grad.data.to(device)
 
-def get_loader(Xtr, Xva, ytr, yva, batch_size = 4):
+def get_loader(Xtr, Xva, ytr, yva, batch_size = 1):
     train_dataset = torch.utils.data.TensorDataset(Xtr, ytr)
     test_dataset = torch.utils.data.TensorDataset(Xva, yva)
     trainloader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=2)
     testloader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=2)
     return trainloader, testloader
 
-def train_process(myDevice, myCNN, loader, EPOCH = 2):
+def train_process(myDevice, myCNN, loader, LR = 0.001, EPOCH = 2, PRINT = 100):
     criterion = nn.CrossEntropyLoss().cuda() if torch.cuda.is_available() else nn.CrossEntropyLoss()
     print(criterion)
-    optimizer = optim.SGD(myCNN.parameters(), lr=0.001, momentum=0.9)
+    optimizer = optim.SGD(myCNN.parameters(), lr=LR, momentum=0.9)
     optimizer_to(optimizer, myDevice)
     for epoch in range(EPOCH):  # loop over the dataset multiple times
         running_loss = 0.0
@@ -111,8 +153,31 @@ def train_process(myDevice, myCNN, loader, EPOCH = 2):
 
             # print statistics
             running_loss += loss.item()
-            if i % 100 == 99:    # print every 100 mini-batches
-                print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 100:.3f}')
+            if i % PRINT == PRINT - 1:    # print every 100 mini-batches
+                print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / PRINT:.3f}')
+                running_loss = 0.0
+    print('Finished Training')
+    return myCNN
+
+def train_process2(myDevice, myCNN, loader, LR = 0.001, EPOCH = 2, PRINT = 100):
+    criterion = nn.CrossEntropyLoss().cuda() if torch.cuda.is_available() else nn.CrossEntropyLoss()
+    print(criterion)
+    optimizer = optim.Adam(myCNN.parameters(), lr=LR)
+    optimizer_to(optimizer, myDevice)
+    for epoch in range(EPOCH):  # loop over the dataset multiple times
+        running_loss = 0.0
+        for i, data in enumerate(loader, 0):
+            inputs, labels = data[0].to(myDevice), data[1].to(myDevice)
+            optimizer.zero_grad()
+            outputs = myCNN(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            # print statistics
+            running_loss += loss.item()
+            if i % PRINT == PRINT - 1:    # print every 100 mini-batches
+                print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / PRINT:.3f}')
                 running_loss = 0.0
     print('Finished Training')
     return myCNN
